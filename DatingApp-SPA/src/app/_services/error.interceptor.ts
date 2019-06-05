5ca900fd419d6158eb67bb42c8640fc8c4474cfe
Jesse Bryan (HCL America Inc) <v-jebrya@microsoft.com>
2019-06-05 14:30:33 -0700

    end of section 6 - added third party components

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -3,6 +3,10 @@ import { HttpInterceptor, HttpRequest, HttpEvent, HttpHandler, HttpErrorResponse
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
+// error handling for login and registration really needs to be revamped later on if it isn't later in the course
+// should add front end error handling to make verify password length prior to sending to db
+// can eventually add username exists check before they click on send - not necessary though, just don't clear fields in this case
+
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -36,7 +40,7 @@ export class ErrorInterceptor implements HttpInterceptor {
     }
 }
 
-// need to export 
+// need to export
 // we are adding our own custom interceptor on top of angular interceptor
 // multi means we add it to existing interceptors rather than replace them
 export const ErrorInterceptorProvider = {
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+import * as alertifyjs from 'alertifyjs';
 
 @Component({
   selector: 'app-nav',
@@ -9,28 +11,34 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
+    // here just for demonstration i have imported alertifyjs without the service just to show that it works
+    // i imagine one of the advantages to creating a service for alertify methods is that we can add additional logic for each method
+    // that way success could do additional things if we wanted it to
+    // it also supports decoupling in general so that if there is something similar to alertify we can just change the service methods
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
+      alertifyjs.success('Logged in successfully new');
+      // this.alertify.success('Logged in successfully');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    // !!  = if exists it is true otherwise it is false - it coerces the value into its boolean truthiness exquivalent
-    return !!token;
+    // const token = localStorage.getItem('token');
+    // // !!  = if exists it is true otherwise it is false - it coerces the value into its boolean truthiness exquivalent
+    // return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('Logged out');
   }
 
 }
