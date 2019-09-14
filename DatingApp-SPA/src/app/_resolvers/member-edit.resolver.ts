eb2a96261c011dc78df0151ce7b03b8669c048bf
jbryan22 <jesse.bryan22@gmail.com>
2019-09-13 22:37:47 -0700

    finished section 10 - edit a profile and update API to allow for updating

--- DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
@@ -1,5 +1,6 @@
+import { AuthService } from './../_services/auth.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
-import { UserService } from './../_services/user.service';
+import { UserService } from '../_services/user.service';
 import { User } from 'src/app/_models/user';
 import { Injectable } from '@angular/core';
 import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
@@ -7,13 +8,14 @@ import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
-export class MemberDetailResolver implements Resolve<User> {
-    constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
+export class MemberEditResolver implements Resolve<User> {
+    constructor(private userService: UserService, private authService: AuthService,
+                private router: Router, private alertify: AlertifyService) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User> {
-        return this.userService.getUser(route.params['id']).pipe(
+        return this.userService.getUser(this.authService.decodedToken.nameid).pipe(
             catchError(error => {
-                this.alertify.error('Problem retrieving data');
+                this.alertify.error('Problem retrieving edit data');
                 this.router.navigate(['/members']);
                 return of(null);
             })
